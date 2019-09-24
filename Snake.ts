6fe4fabf95716f889b9eb704edf4ecddbf53d2f7
jj24126 <jjose2@lions.molloy.edu>
2019-09-24 15:20:59 -0400

    1 file modified, 1 added

--- Snake.ts
+++ Snake.ts
@@ -12,20 +12,24 @@ class Snake{
     this.currentDirection = 1
   }
   public move(numMoves:number){
-    if(this.currentDirection === 1) {this.currentPosition = this.currentPosition + numMoves}
-    else if(this.currentDirection === -1) {this.currentPosition = this.currentPosition - numMoves}
+    if(this.currentDirection === 0) {this.currentPosition = new Point(this.position.x, this.position.y + numMoves)}
+    else if(this.currentDirection === 1) {this.currentPosition = new Point(this.position.x + numMoves, this.position.y)}
+    else if(this.currentDirection === 2) {this.currentPosition = new Point(this.position.x, this.position.y - numMoves)}
+    else if( this.currentDirection === 3) {this.currentPosition = new Point(this.position.x - numMoves, this.position.y)}
   }
-  public turn(){
-    if(this.currentDirection === 1) {this.currentDirection = -1}
-    else if(this.currentDirection === -1) {this.currentDirection = 1}
-  }
-
-  public turnLeft(){
-
+  
+   public turnLeft(){
+    if(this.currentDirection === 0) {this.currentDirection = 3}
+    else if(this.currentDirection === 1) {this.currentDirection = 0}
+    else if(this.currentDirection === 2) {this.currentDirection = 1}
+    else if(this.currentDirection === 3) {this.currentDirection = 2}
   }
 
   public turnRight(){
-    
+    if(this.currentDirection === 0) {this.currentDirection = 1}
+    else if(this.currentDirection === 1) {this.currentDirection = 2}
+    else if(this.currentDirection === 2) {this.currentDirection = 3}
+    else if(this.currentDirection === 3) {this.currentDirection = 0}
   }
 
   public get position(){
---
+++ WorldModel.ts
@@ -0,0 +1,11 @@
+
+class WorldModel{
+
+  constructor(){
+  }
+
+  public update(steps){
+    
+  }
+}
+

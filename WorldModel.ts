e30b70ba09a5f463e1284d43e6837542d85afd9a
jj24126 <jjose2@lions.molloy.edu>
2019-10-10 20:56:20 -0400

    1 file modified

--- WorldModel.ts
+++ WorldModel.ts
@@ -5,11 +5,13 @@ class WorldModel{
   private snk:Snake
   private width
   private height
+  private view
 
  constructor(s:Snake){
     this.snk = s
     this.width
     this.height
+    this.view = null
   }
 
   public update(steps){

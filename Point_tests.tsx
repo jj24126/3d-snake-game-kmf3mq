1db991b6726b793379a802a1b9326f667b6f54aa
jj24126 <jjose2@lions.molloy.edu>
2019-11-07 15:15:23 -0500

    Added Point tests

--- Point_tests.tsx
+++ Point_tests.tsx
@@ -1,5 +1,13 @@
 import Point from './Point'
 
 describe("Point", function(){
-  
+  it("has working getter for y", function(){
+    const p = new Point(4,7)
+    expect(p.y).toBe(7)
+  })
+
+  it("has working x getter", function(){
+    const p = new Point(4,7)
+    expect(p.x).toBe(4)
+  })
 })

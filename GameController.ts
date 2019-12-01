f33f0970b29104627faa5608946baed6a74215af
jj24126 <jjose2@lions.molloy.edu>
2019-12-01 13:29:04 -0500

    1 file modified

--- GameController.ts
+++ GameController.ts
@@ -3,4 +3,26 @@ import WorldModel from './WorldModel'
 
 class GameController{
   private world:WorldModel
+  private player1
+  private player2
+  constructor(w:WorldModel){
+    this.world = w
+  }
+
+  public run(){
+    const lastTime = 0
+  }
+
+  public updateFrame(){
+    requestAnimationFrame(this.updateFrame)
+  }
+
+  public set p1(){
+
+  };
+
+  public set p2(){
+
+  };
+
 }
